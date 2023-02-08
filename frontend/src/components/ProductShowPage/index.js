ce4def46fc0c63ebe27cdd6c428c24ad304dacf3
Luis <perezluis167@gmail.com>
2023-02-07 20:31:23 -0500

    Im able to grab products from the backend

--- frontend/src/components/ProductShowPage/index.js
+++ frontend/src/components/ProductShowPage/index.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Redirect, useParams } from 'react-router-dom/cjs/react-router-dom.min';
-import { fetchProduct,  getProduct, getProducts } from '../../store/product';
+import { fetchProduct, getProduct } from '../../store/product';
 import Navigation from '../Navigation';
 import './ProductShowPage.css'
 
@@ -11,20 +11,40 @@ const ProductShowPage = () => {
     const { productId } = useParams()
     const dispatch = useDispatch();
     const product = useSelector(getProduct(productId))
-    
-    useEffect(()=>{
+
+    useEffect(() => {
         dispatch(fetchProduct(productId))
-            .then(()=> setIsLoading(false))
-    }, [dispatch,productId])
+            .then(() => setIsLoading(false))
+    }, [dispatch, productId])
 
 
-    if (isLoading) return <div>Loading...</div>
-    if (!isLoading && !product) return <Redirect to='/product_does_not_exist'/>
+    if (isLoading) return <h1>Loading...</h1>
+    if (!isLoading && !product) return <Redirect to='/product_does_not_exist' />
 
+    const imageSrc = product ? require(`../../images/${product.image}`) : null
     return (
         <>
-        <Navigation />
-        <div ></div>
+            <Navigation />
+            <div id='topOfPage'>
+                <div id='imageDivLeftSide'>
+                    {imageSrc ? <img id='productImage' src={imageSrc} alt='no img :(' /> : <img src='https://nyc3.digitaloceanspaces.com/memecreator-cdn/media/__processed__/e37/template-fairly-odd-parents-987-0c6db91aec9c.jpeg' alt='this Is not it' />}
+                </div>
+                <div id='midDivForTitleAndStuff'>
+                    <h1 id='productNameH1'>{product.name}</h1>
+                    <ul id='bulletDesc'>
+                        {product.bulletDesc.map((bullet, i) => <li key={`bullet-${i}`}>{bullet}</li>)}
+                    </ul>
+                </div>
+                <div id='rightDivCart'>
+                    
+                </div>
+            </div>
+            <div id='productDescriptionDiv'>
+                <p id='productDescriptionPTag'>{product.description}</p>
+            </div>
+            <div id='ReviewsDiv'>
+
+            </div>
         </>
     )
 }
