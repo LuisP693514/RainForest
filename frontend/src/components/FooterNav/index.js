56d6defc1db03aa8001efa228eda3e653cc10352
Luis <perezluis167@gmail.com>
2023-03-30 15:31:10 -0400

    Fixed syntax errors

--- frontend/src/components/FooterNav/index.js
+++ frontend/src/components/FooterNav/index.js
@@ -1,18 +1,16 @@
-import { Link } from 'react-router-dom';
 import './FooterNav.css'
 
 const FooterNav = () => {
-
 
     return (
         <div id='bottomOfTheBarrel'>
             <div id='inBetweenTags'>
 
-                <a id='footerLinkTags' href='https://www.linkedin.com/in/luis-perez-5baa4695/'>
-                    <img id='gitHubHomePage' src={require(`../../images/linkedInLogo.png`)} alt=''/>
+                <a id='footerLinkTags' href='https://www.linkedin.com/in/luis-perez-5baa4695/' target={"_blank"}>
+                    <img id='gitHubHomePage' src={require(`../../images/linkedInLogo.png`)} alt='' />
                 </a>
-                <a id='footerLinkTags' href='https://github.com/LuisP693514/RainForest'>
-                    <img id='gitHubHomePage' src={require(`../../images/gitHubLogo.png`)} alt=''/>
+                <a id='footerLinkTags' href='https://github.com/LuisP693514/RainForest' target={"_blank"}>
+                    <img id='gitHubHomePage' src={require(`../../images/gitHubLogo.png`)} alt='' />
                 </a>
                 {/* <Link id='footerLinkTags' to='/'>
                     <img id='gitHubHomePage' src={require(`../../images/linkedInLogo.png`)} alt=''/>
